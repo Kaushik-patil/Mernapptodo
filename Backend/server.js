af15536022dad9361210b7872c347f2df0cdb781
Kaushik <kp87030@gmail.com>
2022-08-18 00:24:58 +0530

    first commit

--- Backend/server.js
+++ Backend/server.js
@@ -4,29 +4,77 @@
  npm init :--> git
  installing dependencies and devdependencies
  node mon starts server
- 
  we use require here instead of import.
-
  */
+//  const path = require('path')
+//  const express = require('express')
+//  const colors = require('colors')
+//  const dotenv =require('dotenv').config()
+//   const {errorHandler}=require('./middleware/errorMiddleware')
+//  const connectDB =require('./config/db')
 
- const express = require('express')
- const colors = require('colors')
- const dotenv =require('dotenv').config()
- /*const {errorHandler}=require('./middleware/errorMiddleware')*/
- const connectDB =require('./config/db')
-
- connectDB()
- const port =5000;
-
-
-
- const apps= express()
- apps.use(express.json())
- apps.use(express.urlencoded({extended : false}))
- /*apps.use(errorHandler)*/
-
- apps.use('/api/goals',require('./routes/goalRoutes'))
- apps.use('/api/users',require('./routes/userRoutes'))
+//  connectDB()
+//  const port =5000;
 
 
- apps.listen(port,()=>console.log(`server started at ${port}`)) 
+
+//  const apps= express()
+//  apps.use(express.json())
+//  apps.use(express.urlencoded({extended : false}))
+
+
+//  apps.use('/api/goals',require('./routes/goalRoutes'))
+//  apps.use('/api/users',require('./routes/userRoutes'))
+
+// // Serve frontend
+// if (process.env.NODE_ENV === 'production') {
+//     apps.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+//     apps.get('*', (req, res) =>
+//       res.sendFile(
+//         path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+//       )
+//     );
+//   } else {
+//     apps.get('/', (req, res) => res.send('Please set to production'));
+//   }
+
+//   apps.use(errorHandler)
+
+//  apps.listen(port,()=>console.log(`server started at ${port}`)) 
+
+
+const path = require('path');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+// const {errorHandler}  = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const apps = express();
+
+apps.use(express.json());
+apps.use(express.urlencoded({ extended: false }));
+
+apps.use('/api/goals', require('./routes/goalRoutes'));
+apps.use('/api/users', require('./routes/userRoutes'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  apps.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  apps.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  apps.get('/', (req, res) => res.send('Please set to production'));
+}
+
+// apps.use(errorHandler);
+
+apps.listen(port, () => console.log(`Server started on port ${port}`));
